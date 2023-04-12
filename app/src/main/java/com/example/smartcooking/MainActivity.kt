f8df9b8042707b81c67d75c6d3aaf92b36c635ff
Ali-The-Great <98874288+Ali-The-Great@users.noreply.github.com>
2023-04-12 14:13:23 +0300

    second commit

--- app/src/main/java/com/example/smartcooking/MainActivity.kt
+++ app/src/main/java/com/example/smartcooking/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.smartcooking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.LoginBtn)
+        val registerButton = findViewById<Button>(R.id.registerBtn)
+
+        loginButton.setOnClickListener {
+            // Navigate to the Log in page
+            // You can replace LoginActivity with the name of your login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        registerButton.setOnClickListener {
+            // Navigate to the Register page
+            // You can replace RegisterActivity with the name of your register activity
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
